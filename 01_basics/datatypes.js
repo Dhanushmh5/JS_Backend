9bfa56d2454b822ad599f8332210967b1b42afb4
Maxi <dhanushmh5@gmail.com>
2025-09-22 11:15:41 +0000

    Datatypes and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+//alert() cannot be used in nodejs not browwser
+//code reusabiltiy should be high
+
+
+/*
+primitive datatypes:
+number
+bigint
+string=> "" or ''
+boolean
+symbol
+null=>standalone value
+undefined=>return undefined when checked with typeoff operator
+complex datatype:
+objects
+*/
+console.log(typeof null)
---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accId=1334466
+let userEmail="dhanush@yahoo.com"
+var password="Dhanushmh5@"
+city="Koppal"
+// accId=2
+userEmail="abc@abc.com"
+password="12345678"
+city="Dubai"
+//prefferd not to use var 
+let abc;
+
+console.log(accId);
+console.log(userEmail);
+console.log(password);
+console.log(city);
+console.table([accId,userEmail,password,city,abc]);

92c9b4f5f8496394e9a0da716352f2b604ff77d7
Maxi <dhanushmh5@gmail.com>
2025-10-08 18:17:16 +0000

    Objects and Arrays in functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -23,3 +23,46 @@ console.log(status)
 
 const status1=loggedIn() // returns an undefines if value is not passed to avoid use an if check
 console.log(status1)
+
+///////////////PART2
+
+function calculateCartPrice(a){
+    return a
+}
+console.log(calculateCartPrice(200,300,400)) //only first value gets printed since only expects one parameter
+
+function calculateCartPrice(a,b,...num1){//rest operator
+    return num1
+}
+console.log(calculateCartPrice(200,300,400,300,500,600)) //200-a 300-b [400,300,500,600]->num1
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
